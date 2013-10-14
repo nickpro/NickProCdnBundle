ac042df4fd365d04b8446115600adef249c8f2e3
nickpro <n.prokurat@turboheads.by>
2013-10-14 17:44:03 +0300

    cut useless comments

--- Filemanager/Storage/Local/Container.php
+++ Filemanager/Storage/Local/Container.php
@@ -55,12 +55,6 @@ class Container implements ContainerInterface
         $this->originalFileName = $this->fileName;
         
         if ($dimension) {
-//            $this->fileName .= self::$delimiter . $dimension;
-//            $dotPosition = strrpos($this->fileName, '.');
-//            $fileName = substr($this->fileName, 0, $dotPosition);
-//            $ext = substr($this->fileName, $dotPosition + 1);
-//            
-//            $this->fileName = $fileName . $dimension . self::$delimiter . '.' . $ext;
             $this->fileName = $this->getPathWithDimension($this->fileName, $dimension);
         }
         
